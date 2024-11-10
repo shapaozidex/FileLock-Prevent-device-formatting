510cd6255ce6d3d922bbb7ad2e6bc0e685fd6662
Yuuki <39391923+SoyBeanMilkx@users.noreply.github.com>
2024-11-10 14:49:43 +0800

    Delete src directory

--- src/data_parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYS_SECURITY_DATA_PARSE_H
-#define SYS_SECURITY_DATA_PARSE_H
-
-#include <linux/string.h>
-#include "file.h"
-
-void parse_paths(char *args, char paths[MAX_PATHS][PATH_MAX], int *path_count);
-
-#endif
--- src/log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    adb shell "su -c 'dmesg -w | grep $TAG'"
-    echo
-    read -p "Press [Enter] key to continue..."
-done
